16175277538a177d60da9e3b5946416c40c34e95
Luke Lau <luke_lau@icloud.com>
2020-06-30 16:57:25 +0100

    Start work on contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,55 @@
 import React from 'react'
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
+import Contact from './Contact'
 
 function App() {
   return (
     <Router>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">PunkBuddy</a>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">PunkBuddy</a>
 
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarItems">
-              <span className="navbar-toggler-icon"></span>
-            </button>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarItems">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-            <div className="collapse navbar-collapse" id="navbarItems">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link to="/writeAndListen" className="nav-link">Write and Listen</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/writeAndCheck" className="nav-link">Write and Check</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/practise" className="nav-link">Practise</Link>
-                </li>
-              </ul>
-            </div>
+          <div className="collapse navbar-collapse" id="navbarItems">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/writeAndListen" className="nav-link">Write and Listen</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/writeAndCheck" className="nav-link">Write and Check</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/practise" className="nav-link">Practise</Link>
+              </li>
+            </ul>
           </div>
+        </div>
       </nav>
 
-      <Switch>
-        <Route path="/(writeAndListen)?">
-          Write and listen
+      <div className="container">
+        <Switch>
+          <Route path="/writeAndListen">
+            Write and listen
         </Route>
-        <Route path="/writeAndCheck">
-          Write and check
+          <Route path="/writeAndCheck">
+            Write and check
         </Route>
-        <Route path="/practise">
-          Practise
+          <Route path="/practise">
+            Practise
         </Route>
-      </Switch>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </div>
+
+      <footer>
+        <div className="container">
+          <Link to="/contact" className="text-muted">Contact</Link></div>
+      </footer>
     </Router>
   )
 }
---
+++ src/Contact.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+export default () => {
+  return (
+    <div>
+      <p>
+        Punk Buddy is an artefact created at Trinity College Dublin by Chara Triantafyllidou.
+        It's goal is to assist learners with writing and reading difficulties, as well as their parents and their English teachers.
+        Recommended for students in the final grade of Primary School onwards.
+        Leave your feedback below! Thanks for visiting!
+      </p>
+    </div>
+  )
+}
