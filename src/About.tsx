f5eebaf5d4cdd4c255e8b1ec0315026d9e7d36df
Luke Lau <luke_lau@icloud.com>
2020-08-21 14:29:32 +0100

    Remove dummy forms from about page

--- src/About.tsx
+++ src/About.tsx
@@ -1,22 +1,17 @@
 import React from 'react'
 export default () => {
   return (
-    <div>
-      <p>
-        PunkBuddy is an artefact created at Trinity College Dublin by Luke Lau and Chara Triantafyllidou. It is part of the dissertation of Chara Triantafyllidou for the MPhil in Applied Linguistics, 2019-2020.
-        Its goal is to assist learners with writing and reading difficulties, as well as their parents and their English teachers. PunkBuddy yields the affordances of TTS technologies for proof-listening and uses a punctuation correction interface. Its design is informed by recent neurocognitive findings on prosodic processing and dyslexia, a perception test on punctuation, a text analysis of Junior Cycle textbooks, and the design of recent dyslexia software.
-        Recommended for students in the final grade of Primary School onwards.
-      </p>
-      <form>
-        <div className="mb-3">
-          <label htmlFor="emailAddress" className="form-label">Email address</label>
-          <input type="html" className="form-control" id="emailAddress" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="emailAddress" className="form-label">Email address</label>
-          <input type="html" className="form-control" id="emailAddress" />
-        </div>
-      </form>
+    <div className="row">
+      <div className="col-12 col-lg-4">
+        <h1>About</h1>
+      </div>
+      <div className="col-12 col-lg-8">
+        <p>
+          PunkBuddy is an artefact created at Trinity College Dublin by Luke Lau and Chara Triantafyllidou. It is part of the dissertation of Chara Triantafyllidou for the MPhil in Applied Linguistics, 2019-2020.
+          Its goal is to assist learners with writing and reading difficulties, as well as their parents and their English teachers. PunkBuddy yields the affordances of TTS technologies for proof-listening and uses a punctuation correction interface. Its design is informed by recent neurocognitive findings on prosodic processing and dyslexia, a perception test on punctuation, a text analysis of Junior Cycle textbooks, and the design of recent dyslexia software.
+          Recommended for students in the final grade of Primary School onwards.
+        </p>
+      </div>
     </div>
   )
 }
