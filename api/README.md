9679632915511852d871390ea7d69c0a2fcfc2d3
Luke Lau <luke_lau@icloud.com>
2020-08-22 20:33:59 +0100

    Add adjective analysis API

---
+++ api/README.md
@@ -0,0 +1,1 @@
+To use, put the service account json key contents into the `GOOGLE_CLOUD_CREDENTIALS` environment variable
---
+++ api/analysis.rb
@@ -0,0 +1,36 @@
+require 'google/cloud/language'
+require 'json'
+
+language = Google::Cloud::Language.language_service
+
+Handler = Proc.new do |req, res|
+
+  text = req.query['text'].to_s
+  document = { content: text, type: :PLAIN_TEXT }
+
+  syntax_res = language.analyze_syntax document: document
+
+  adjectives = []
+
+  cur_char = 0
+  cur_text = text
+
+  for token in syntax_res.tokens
+    # p token.text.content
+    offset = token.text.begin_offset
+    length = token.text.content.length
+    if token.part_of_speech.tag == :ADJ
+      adjectives << {:offset => cur_char, :length => length}
+    end
+    
+    # now move on to the next word
+    matches = /\b.+?\b.+?\b/.match(cur_text)
+    if matches != nil
+      to_trim = matches[0].length
+      cur_char += to_trim
+      cur_text = cur_text[to_trim..-1]
+    end
+  end
+
+  res.body = JSON.generate({:adjectives => adjectives})
+end
